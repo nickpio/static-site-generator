8de292301e98585c4035ea5de28f666ec13b3337
nickpio <nickp1o@pm.me>
2026-04-15 13:41:12 -0700

    LeafNode created

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,11 +11,38 @@ class HTMLNode():
         raise NotImplementedError
 
     def props_to_html(self):
+        props_list = []
         if self.props == None or self.props == "":
             return ""
 
         for item in self.props:
-             return f'{item}="{self.props[item]}" '
+             props_list.append(f' {item}="{self.props[item]}"')
+
+        return props_list
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All Leaf Nodes must have a value")
+
+        if self.tag == None:
+            return self.value
+
+        html_props = self.props_to_html()
+        html = f"<{self.tag}"
+        for prop in html_props:
+            string = f"{prop}"
+            html += string
+
+        html += f">{self.value}</{self.tag}>"
+        return html
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,11 +3,15 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
         node = HTMLNode("a", "test", [], {"href": "https://www.google.com"})
         print(node.props_to_html())
 
+    def test_leaf(self):
+        node = LeafNode("a", "test", {"href": "https://www.google.com"})
+        print(node.to_html())
 if __name__ == "__main__":
     unittest.main()
